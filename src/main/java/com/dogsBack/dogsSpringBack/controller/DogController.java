fef00829dfd39ccd34f467e8658d1582610d7303
Luis david <david12.29@hotmail.com>
2023-08-13 17:26:37 -0500

    Update the DogController.Now we can use get and post in postman

--- src/main/java/com/dogsBack/dogsSpringBack/controller/DogController.java
+++ src/main/java/com/dogsBack/dogsSpringBack/controller/DogController.java
@@ -1,9 +1,11 @@
 package com.dogsBack.dogsSpringBack.controller;
 
+import com.dogsBack.dogsSpringBack.model.Dog;
 import com.dogsBack.dogsSpringBack.repository.DogRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @CrossOrigin("http://localhost:3000")
@@ -11,4 +13,14 @@ public class DogController {
 
     @Autowired
     private DogRepository dogsRepository;
+
+    @PostMapping("/dog")
+    Dog newUser(@RequestBody Dog newDog){
+        return dogsRepository.save(newDog);
+    }
+    @GetMapping("/dogs")
+    List<Dog> getAllUsers(){
+        return dogsRepository.findAll();
+    }
+
 }
